320b233bdc4b22d49d464a942ef68c245162a030
Langelozzi <lucasangelozzi32@gmail.com>
2023-01-07 15:33:57 -0800

    fix response wait

--- front-end/src/components/Recipe/Recipe.js
+++ front-end/src/components/Recipe/Recipe.js
@@ -6,47 +6,43 @@ import Image from 'react-bootstrap/Image'
 export default function Recipe() {
   const [recipe, setRecipe] = React.useState({});
 
-  function removeTags(string) {
-    console.log(recipe)
-    console.log(string)
-    return string.replace(/<\/?[^>]+(>|$)/g, "")
-  }
-
-  function getNameFromJson(list) {
-    let list_of_items = []
-    for (let element of list) {
-      list_of_items.push(element.name)
-    }
-    return list_of_items
-  }
-
   React.useEffect(() => {
     async function loadRecipe() {
       const data = await getRecipeById(641794);
+      data.summary = data.summary.replace(/<\/?[^>]+(>|$)/g, "");
       setRecipe(data);
     }
     loadRecipe();
   }, [])
 
+  let ingredientsToRender;
+  if (recipe.extendedIngredients) {
+    ingredientsToRender = recipe.extendedIngredients.map(ingredient => {
+      return <div key={ingredient.id}>{ingredient.name}</div>;
+    });
+  }
+
   return (
     <div>
-      <div class="header">
-        <h1 class="centre">{recipe.title}</h1>
-        <img class="mealPicture centerImage" src={recipe.image}></img>
+      <div className="header">
+        <h1 className="centre">{recipe.title}</h1>
+        <img className="mealPicture centerImage" src={recipe.image}></img>
       </div>
 
-      <div class="description">
-        <h2 class="left">Description</h2>
-        <p>{removeTags(recipe.summary)}</p>
+      <div className="description">
+        <h2 className="left">Description</h2>
+        <p>{recipe.summary}</p>
       </div>
 
-      <div class="ingredients">
-        <h2 class="left">Ingredients</h2>
-        <p>{getNameFromJson(recipe.ingredients)}</p>
+      <div className="ingredients">
+        <h2 className="left">Ingredients</h2>
+        {
+          ingredientsToRender
+        }
       </div>
 
-      <div class="instructions">
-        <h2 class="left">Instructions</h2>
+      <div className="instructions">
+        <h2 className="left">Instructions</h2>
       </div>
 
 
