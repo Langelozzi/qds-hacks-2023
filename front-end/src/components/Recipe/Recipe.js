92ef5b333a6f19fe019d649b0a7ad0f3395d10be
ChinaiArman <chinaiarman@gmail.com>
2023-01-07 20:01:46 -0800

    Add content body

--- front-end/src/components/Recipe/Recipe.js
+++ front-end/src/components/Recipe/Recipe.js
@@ -4,11 +4,14 @@ import Image from 'react-bootstrap/Image'
 import { useLocation } from 'react-router-dom';
 import { FaRegClock } from "react-icons/fa";
 import { BsPeopleFill } from "react-icons/bs"
-import { GrTarget } from "react-icons/gr"
+import { TbTarget } from "react-icons/tb"
+import Tab from 'react-bootstrap/Tab';
+import Tabs from 'react-bootstrap/Tabs';
+
 
 export default function Recipe() {
-    const location = useLocation();
-    const recipe = location.state?.recipe;
+  const location = useLocation();
+  const recipe = location.state?.recipe;
 
   let ingredientsToRender;
   if (recipe.extendedIngredients) {
@@ -29,12 +32,32 @@ export default function Recipe() {
       <div className="header">
         <h1 className="centre">{recipe.title}</h1>
         <div id="headerMenu">
-          <div class="menuitem"><FaRegClock></FaRegClock>&nbsp;&nbsp;{recipe.readyInMinutes}</div>
-          <div class="menuitem"><BsPeopleFill></BsPeopleFill>&nbsp;&nbsp;{recipe.servings}</div>
-          <div class="menuitem"><GrTarget></GrTarget>&nbsp;&nbsp;{recipe.matchScore * 100}% match</div>
+          <div className="menuitem"><FaRegClock></FaRegClock>&nbsp;&nbsp;{recipe.readyInMinutes}</div>
+          <div className="menuitem" id="centrepiece"><BsPeopleFill></BsPeopleFill>&nbsp;&nbsp;{recipe.servings}</div>
+          <div className="menuitem"><TbTarget></TbTarget>&nbsp;&nbsp;{recipe.matchScore * 100}% match</div>
         </div>
       </div>
 
+      <main>
+        <div className="recipeContent">
+          <Tabs
+            defaultActiveKey="profile"
+            id="uncontrolled-tab-example"
+            className="mb-3"
+          >
+            <Tab eventKey="description" title="Description">
+            </Tab>
+            <Tab eventKey="ingredients" title="Ingredients">
+            </Tab>
+            <Tab eventKey="instructions" title="Instructions">
+            </Tab>
+          </Tabs>
+        </div>
+
+        <div className="recipeImage">
+          <img className="mealPicture centerImage" src={recipe.image}></img>
+        </div>
+      </main>
       {/* <img className="mealPicture centerImage" src={recipe.image}></img>
       <div className="description">
         <h2 className="left">Description</h2>
