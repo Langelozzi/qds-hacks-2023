d2d9cff3648f8858ab6e45386cc68ddbb8b1de52
annettelam <113328885+annettelam@users.noreply.github.com>
2023-01-07 16:11:20 -0800

    Add hamburger nav bootstrap

--- front-end/src/components/Nav/Nav.js
+++ front-end/src/components/Nav/Nav.js
@@ -1,13 +1,129 @@
-import React from 'react'
 import './Nav.css';
-import { Squash as Hamburger } from 'hamburger-react';
+import React, { useState } from 'react';
+import {
+  MDBContainer,
+  MDBCollapse,
+  MDBNavbar,
+  MDBNavbarToggler,
+  MDBIcon,
+  MDBBtn,
+} from 'mdb-react-ui-kit';
 
-export default function Nav() {
+export default function App() {
+  const [showAnimated, setShowAnimated] = useState(false);
+  const [showAnimated2, setShowAnimated2] = useState(false);
+  const [showAnimated3, setShowAnimated3] = useState(false);
+
   return (
-    <div>
-      <Hamburger/>
-    </div>
+    <>
+      <section className='mb-3'>
+        <MDBNavbar>
+          <MDBContainer fluid>
+            <MDBNavbarToggler
+              type='button'
+              className='first-button'
+              data-target='#navbarToggleExternalContent'
+              aria-controls='navbarToggleExternalContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+              onClick={() => setShowAnimated(!showAnimated)}
+            >
+              <div className={`animated-icon1 ${showAnimated && 'open'}`}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </MDBNavbarToggler>
+          </MDBContainer>
+        </MDBNavbar>
 
+        <MDBCollapse show={showAnimated}>
+          <div className='bg-light shadow-3 p-4'>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 1
+            </MDBBtn>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 2
+            </MDBBtn>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 2
+            </MDBBtn>
+          </div>
+        </MDBCollapse>
+      </section>
 
-  )
+      <section className='mb-3'>
+        <MDBNavbar dark bgColor='dark'>
+          <MDBContainer fluid>
+            <MDBNavbarToggler
+              type='button'
+              className='second-button'
+              data-target='#navbarToggleExternalContent'
+              aria-controls='navbarToggleExternalContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+              onClick={() => setShowAnimated2(!showAnimated2)}
+            >
+              <div className={`animated-icon2 ${showAnimated2 && 'open'}`}>
+                <span></span>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </MDBNavbarToggler>
+          </MDBContainer>
+        </MDBNavbar>
+
+        <MDBCollapse show={showAnimated2}>
+          <div className='bg-light shadow-3 p-4'>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 1
+            </MDBBtn>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 2
+            </MDBBtn>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 2
+            </MDBBtn>
+          </div>
+        </MDBCollapse>
+      </section>
+
+      <section className='mb-3'>
+        <MDBNavbar dark bgColor='info'>
+          <MDBContainer fluid>
+            <MDBNavbarToggler
+              type='button'
+              className='third-button'
+              data-target='#navbarToggleExternalContent'
+              aria-controls='navbarToggleExternalContent'
+              aria-expanded='false'
+              aria-label='Toggle navigation'
+              onClick={() => setShowAnimated3(!showAnimated3)}
+            >
+              <div className={`animated-icon3 ${showAnimated3 && 'open'}`}>
+                <span></span>
+                <span></span>
+                <span></span>
+              </div>
+            </MDBNavbarToggler>
+          </MDBContainer>
+        </MDBNavbar>
+
+        <MDBCollapse show={showAnimated3}>
+          <div className='bg-light shadow-3 p-4'>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 1
+            </MDBBtn>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 2
+            </MDBBtn>
+            <MDBBtn block className='border-bottom m-0' color='link'>
+              Link 2
+            </MDBBtn>
+          </div>
+        </MDBCollapse>
+      </section>
+    </>
+  );
 }
