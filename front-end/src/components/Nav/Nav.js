68fcf54153916e802b9082eb6bfd0dc3eab9bf9a
annettelam <113328885+annettelam@users.noreply.github.com>
2023-01-07 16:16:32 -0800

    Remove 2 extra hamburger menus

--- front-end/src/components/Nav/Nav.js
+++ front-end/src/components/Nav/Nav.js
@@ -11,8 +11,6 @@ import {
 
 export default function App() {
   const [showAnimated, setShowAnimated] = useState(false);
-  const [showAnimated2, setShowAnimated2] = useState(false);
-  const [showAnimated3, setShowAnimated3] = useState(false);
 
   return (
     <>
@@ -40,86 +38,13 @@ export default function App() {
         <MDBCollapse show={showAnimated}>
           <div className='bg-light shadow-3 p-4'>
             <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 1
+              Popular
             </MDBBtn>
             <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 2
+              Healthy
             </MDBBtn>
             <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 2
-            </MDBBtn>
-          </div>
-        </MDBCollapse>
-      </section>
-
-      <section className='mb-3'>
-        <MDBNavbar dark bgColor='dark'>
-          <MDBContainer fluid>
-            <MDBNavbarToggler
-              type='button'
-              className='second-button'
-              data-target='#navbarToggleExternalContent'
-              aria-controls='navbarToggleExternalContent'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
-              onClick={() => setShowAnimated2(!showAnimated2)}
-            >
-              <div className={`animated-icon2 ${showAnimated2 && 'open'}`}>
-                <span></span>
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </MDBNavbarToggler>
-          </MDBContainer>
-        </MDBNavbar>
-
-        <MDBCollapse show={showAnimated2}>
-          <div className='bg-light shadow-3 p-4'>
-            <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 1
-            </MDBBtn>
-            <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 2
-            </MDBBtn>
-            <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 2
-            </MDBBtn>
-          </div>
-        </MDBCollapse>
-      </section>
-
-      <section className='mb-3'>
-        <MDBNavbar dark bgColor='info'>
-          <MDBContainer fluid>
-            <MDBNavbarToggler
-              type='button'
-              className='third-button'
-              data-target='#navbarToggleExternalContent'
-              aria-controls='navbarToggleExternalContent'
-              aria-expanded='false'
-              aria-label='Toggle navigation'
-              onClick={() => setShowAnimated3(!showAnimated3)}
-            >
-              <div className={`animated-icon3 ${showAnimated3 && 'open'}`}>
-                <span></span>
-                <span></span>
-                <span></span>
-              </div>
-            </MDBNavbarToggler>
-          </MDBContainer>
-        </MDBNavbar>
-
-        <MDBCollapse show={showAnimated3}>
-          <div className='bg-light shadow-3 p-4'>
-            <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 1
-            </MDBBtn>
-            <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 2
-            </MDBBtn>
-            <MDBBtn block className='border-bottom m-0' color='link'>
-              Link 2
+              Cheap
             </MDBBtn>
           </div>
         </MDBCollapse>
