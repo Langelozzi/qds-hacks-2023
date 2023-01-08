b5a542064a1c8b59ae16cc1e287b64540c2a037d
Amir Eskandari <113070116+am-eskandari@users.noreply.github.com>
2023-01-07 18:47:27 -0800

    change App js and css

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,37 +1,40 @@
 // React imports
-import React from 'react';
-import { BrowserRouter as Router, Routes, Route, useParams } from 'react-router-dom';
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 // Import css
-import './App.css';
+import "./App.css";
 // Import services
-import { getAllRecipes } from './services/recipe.service';
+import { getAllRecipes } from "./services/recipe.service";
 // Import components
-import Nav from './components/Nav/Nav';
-import Home from './components/Home/Home';
-import Recipe from './components/Recipe/Recipe';
+import Nav from "./components/Navbar/Navbar";
+import Home from "./components/Home/Home";
+import Recipe from "./components/Recipe/Recipe";
 
 function App() {
-    const [recipes, setRecipes] = React.useState([]);
+  const [recipes, setRecipes] = React.useState([]);
 
-    React.useEffect(() => {
-        async function loadRecipes() {
-            const data = await getAllRecipes();
-            setRecipes(data);
-        }
-        loadRecipes();
-    }, [])
+  React.useEffect(() => {
+    async function loadRecipes() {
+      const data = await getAllRecipes();
+      setRecipes(data);
+    }
+    loadRecipes();
+  }, []);
 
-    return (
-        <Router>
-            <Nav />
-            <div className='content'>
-                <Routes>
-                    <Route path='/' element={<Home recipes={recipes} setRecipes={setRecipes} />} />
-                    <Route path='/recipe' element={<Recipe />} />
-                </Routes>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <Nav />
+      <div className="content">
+        <Routes>
+          <Route
+            path="/"
+            element={<Home recipes={recipes} setRecipes={setRecipes} />}
+          />
+          <Route path="/recipe" element={<Recipe />} />
+        </Routes>
+      </div>
+    </Router>
+  );
 }
 
 export default App;
